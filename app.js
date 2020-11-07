217abb43720e02d704b58a7e0dc2d929576994d3
Archana Gottipaty <archana.gottipaty@gmail.com>
2020-11-07 15:43:40 -0800

    made the button functionality better

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ $("form").on("submit", function (e)
    let myButton = "<form><input type=submit value="+cityName1+" style='width:100%'></form>";
 
   element1.innerHTML = myButton;
+  element1.setAttribute("id", cityName1);
   element.append(element1);
    list.prepend(element);
 
@@ -70,6 +71,86 @@ $("form").on("submit", function (e)
       console.log("new URL1:", URL1);
       console.log("new lat*: " + Lat);
       console.log(" new lon*: " + Lon);
+  
+    fetch(URL1, {
+      // The browser fetches the resource from the remote server without first looking in the cache.
+      // The browser will then update the cache with the downloaded resource.
+      // cache: 'reload',
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log("inside fetch of URL1: ");
+        console.log(data);
+        displayFiveDay(data);
+      });
+    });
+
+
+    $("form").on("submit", function (e) 
+{
+  e.preventDefault();
+  cityName = e.target.querySelector("input").value;
+  cityName1 = JSON.stringify(cityName);
+  //  //console.log("city to be searched" + JSON.stringify(citytobeSearched2));
+  //  var citytobeSearched2 = e.target.querySelector("input").value;
+  //console.log(cityName1);
+
+  //console.log("outside" + cityName1);
+
+  let list = document.querySelector("#searchedCities");
+  
+  let element = document.createElement("tr");
+  let element1 = document.createElement("td");
+  // let element2 = document.createElement("button");
+
+   let myButton = "<form><input type=submit value="+cityName1+" style='width:100%'></form>";
+
+  element1.innerHTML = myButton;
+  element.append(element1);
+   list.prepend(element);
+
+  var key = "e3cdabf7296ad5ad27b92f8faad0a9a8";
+
+  // var URL = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`
+  // var URL = `https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=e3cdabf7296ad5ad27b92f8faad0a9a8`
+  var URL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    " + &units=imperial&appid=e3cdabf7296ad5ad27b92f8faad0a9a8";
+
+  fetch(URL, {
+    // The browser fetches the resource from the remote server without first looking in the cache.
+    // The browser will then update the cache with the downloaded resource.
+    // cache: 'reload',
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //console.log("inside fetch: " + data);
+      var dataS = JSON.stringify(data);
+      console.log("inside fetch string: ");
+      console.log(dataS);
+      getCoordinates(data);
+      display(data);
+
+      // TODO: move code here
+    }).then(() => {
+      var URL1 =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      Lat +
+      "&lon=" +
+      Lon +
+      "&units=" + "imperial"+
+      "&exclude=" +
+      part +
+      "&appid=e3cdabf7296ad5ad27b92f8faad0a9a8";
+      console.log("new URL1:", URL1);
+      console.log("new lat*: " + Lat);
+      console.log(" new lon*: " + Lon);
+    });
   
     fetch(URL1, {
       // The browser fetches the resource from the remote server without first looking in the cache.
